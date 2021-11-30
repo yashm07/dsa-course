ae3698619d7a5fe6c250fb5b40dec99f6363b6eb
Yash Matharu <ymatharu7@gmail.com>
2021-11-29 20:23:59 -0500

    Complete singly and doubly linked list implementation

--- LinkedLists/doublyLinkedList.js
+++ LinkedLists/doublyLinkedList.js
@@ -2,14 +2,16 @@ class Node {
   constructor(value) {
     this.value = value;
     this.next = null;
+    this.prev = null;
   }
 }
 
-class LinkedList {
+class DoublyLinkedList {
   constructor(value) {
     this.head = {
       value: value,
-      next: null
+      next: null,
+      prev: null
     }
     this.tail = this.head;
     this.length = 1;
@@ -17,6 +19,8 @@ class LinkedList {
 
   append(value) {
     const newNode = new Node(value);
+    // prev pointer in newnode points to original tail value
+    newNode.prev = this.tail;
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
@@ -26,6 +30,8 @@ class LinkedList {
   prepend(value) {
     const newNode = new Node(value);
     newNode.next = this.head;
+    // the old head prev points to newNode
+    this.head.prev = newNode;
     this.head = newNode;
     this.length++;
     return this;
@@ -52,11 +58,15 @@ class LinkedList {
     // need to get node first before inserting 
     const leader = this.tranverseToIndex(index-1);
     // store pointer
-    const holdingPointer = leader.next;
+    const follower = leader.next;
     // set previous pointer to new node address
     leader.next = newNode;
+    // point newNode backwards to previous node
+    newNode.prev = leader;
+    // point follower prev to newNode
+    follower.prev = newNode;
     // point newNode to the stored pointer
-    newNode.next = holdingPointer;
+    newNode.next = follower;
     this.length++;
     return this.printList();
   }
@@ -72,9 +82,17 @@ class LinkedList {
   }
 
   remove(index) {
+    // access leader before index, to remove current index
     const leader = this.tranverseToIndex(index-1);
+    // access node to delete
     const unwantedNode = leader.next;
-    leader.next = unwantedNode.next;
+    // access follower
+    const follower = unwantedNode.next;
+    // point leader to follower, skipping unwantedNode
+    leader.next = follower;
+    // point follower prev to leader, skipping unwantedNode
+    follower.prev = leader;
+
     // unwantedNode deleted by garbage collection since nothing points to it
     this.length--;
     return this.printList();
@@ -85,7 +103,7 @@ class LinkedList {
 
 }
 
-const myLinkedList = new LinkedList(10);
+const myLinkedList = new DoublyLinkedList(10);
 myLinkedList.append(7);
 myLinkedList.append(20);
 myLinkedList.prepend(1);
